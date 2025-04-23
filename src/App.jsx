db5cf73a31895ac878a7b4748e93dbf21467c4b7
Chadwick Koo <chadwickkoo@outlook.com>
2025-04-23 12:48:00 +0300

    Added react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,  BrowserRouter as Router, Routes, Route }  from 'react'
+import Commits from './components/Commits';
+import ErrorMessage from './components/ErrorMessage';
+import Followers from './components/Followers';
+import Following from './components/Following';
+import ProfileOverview from './components/ProfileOverview';
+import RepoCard from './components/RepoCard';
+import SearchBar from './components/SearchBar';
+import SearchHistory from './components/SearchHistory';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SearchBar />} />
+          <Route path="/commits" element={<Commits />} />
+          <Route path="/error" element={<ErrorMessage />} />
+          <Route path="/followers" element={<Followers />} />
+          <Route path="/following" element={<Following />} />
+          <Route path="/profile" element={<ProfileOverview />} />
+          <Route path="/repo" element={<RepoCard />} />
+          <Route path="/history" element={<SearchHistory />} />
+        </Routes>
+      </Router>
+
+    </div>
   )
 }
 
-export default App
+export default App;
