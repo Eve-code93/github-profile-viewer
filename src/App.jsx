bca04664d716a50657cd56216170bbc554d5556a
Chadwick Koo <chadwickkoo@outlook.com>
2025-04-23 13:16:58 +0300

    Added home and about components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState,  BrowserRouter as Router, Routes, Route }  from 'react'
-import Commits from './components/Commits';
-import ErrorMessage from './components/ErrorMessage';
-import Followers from './components/Followers';
-import Following from './components/Following';
-import ProfileOverview from './components/ProfileOverview';
-import RepoCard from './components/RepoCard';
-import SearchBar from './components/SearchBar';
-import SearchHistory from './components/SearchHistory';
-import './App.css'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './Home'; 
+import ProfileOverview from './ProfileOverview';
+import About from './About';
+
 
 function App() {
   
@@ -16,14 +13,9 @@ function App() {
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<SearchBar />} />
-          <Route path="/commits" element={<Commits />} />
-          <Route path="/error" element={<ErrorMessage />} />
-          <Route path="/followers" element={<Followers />} />
-          <Route path="/following" element={<Following />} />
-          <Route path="/profile" element={<ProfileOverview />} />
-          <Route path="/repo" element={<RepoCard />} />
-          <Route path="/history" element={<SearchHistory />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<ProfileOverview />}/>
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
 
