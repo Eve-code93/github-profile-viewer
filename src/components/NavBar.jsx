097d451a79cb183f8616d530283e0461ffbc5666
Chadwick Koo <chadwickkoo@outlook.com>
2025-04-24 12:21:29 +0300

    modified navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="text-center mt-2">
-      <Link to="/">Home</Link> |{' '}
-      <Link to="/about">About</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-lg">
+      <div className="container">
+        {/* Navbar toggler for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        {/* Links aligned to the left */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto"> {/* me-auto aligns the nav items to the left */}
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                className="nav-link fs-5 text-white mx-3 py-2 px-4 rounded-3 hover-effect"
+                activeClassName="active"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                className="nav-link fs-5 text-white mx-3 py-2 px-4 rounded-3 hover-effect"
+                activeClassName="active"
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
